f5d84cf60f506dfed4ab1cb7ca56a1a6a2cbb8d4
LachlanH <lokthelok@gmail.com>
2019-06-27 23:15:34 +0930

    Changed credentials.h to automatically be included if present, rather than requiring uncommenting. Also added a credentials-example.h

---
+++ ReminderV2/credentials-example.h
@@ -0,0 +1,10 @@
+//Credentials File
+//Do not commit this file for your security!
+
+//WiFi Credentials
+#define mySSID = "............"
+#define myPASSWORD = "............"
+
+//Google Apps Script ID
+#define GoogleScriptIdRead = "............"
+#define GoogleScriptIdWrite = "............"
